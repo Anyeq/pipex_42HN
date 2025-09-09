24033dfaa90958cff3277c920ad8b3caf7a3edc0
asando <alexando17@yahoo.com>
2025-09-09 17:16:08 +0200

    finishing bonus part for multiple pipe no here_doc

--- src/pipex_bonus.c
+++ src/pipex_bonus.c
@@ -6,49 +6,118 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 13:53:05 by asando            #+#    #+#             */
-/*   Updated: 2025/09/09 14:52:57 by asando           ###   ########.fr       */
+/*   Updated: 2025/09/09 17:15:07 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
 
-static void	child_p(char **argv, int **fds, char **envp, int i)
+static int	first_child_process(char **argv, int **fds)
 {
-	int	j;
 	int	file_fd;
-	int	len_argv;
-	j = 0;
 
 	file_fd = 0;
+	file_fd = open_file(argv[1], OF_RD);
+	if (file_fd == -1)
+		return (-1);
+	if (dup2(file_fd, STDIN_FILENO) == -1)
+		return (-1);
+	if (dup2(fds[0][1], STDOUT_FILENO) == -1)
+		return (-1);
+	close(file_fd);
+	return (0);
+}
+
+static int	last_child_process(char **argv, int **fds, int i)
+{
+	int	file_fd;
+	int	len_argv;
+
 	len_argv = 0;
 	while (argv[len_argv])
 		len_argv++;
-	if (i == 0)
+	file_fd = 0;
+	file_fd = open_file(argv[len_argv - 1], OF_TR);
+	if (file_fd == -1)
+		return (-1);
+	if (dup2(fds[i][0], STDIN_FILENO) == -1)
+		return (-1);
+	if (dup2(file_fd, STDOUT_FILENO) == -1)
+		return (-1);
+	close(file_fd);
+	return (0);
+}
+
+static int	middle_child_process(int **fds, int i)
+{
+	if (dup2(fds[i][0], STDIN_FILENO) == -1)
+		return (-1);
+	if (dup2(fds[i][1], STDOUT_FILENO) == -1)
+		return (-1);
+	return (0);
+}
+
+static void	child_p(char **argv, int **fds, int n_pipes, int i)
+{
+	int	file_fd;
+
+	file_fd = 0;
+	if (i == 0 && first_child_process(argv, fds) == -1)
 	{
-		file_fd = open_file(argv[1], OF_RD);
-		if (file_fd == -1)
-		{
-			close_fds(fds);
-			err_exit();
-		}
-		dup2(file_fd, STDIN_FILENO);
-		dup2(fds[i][1], STDOUT_FILENO);
+		close_fds(fds);
+		err_exit();
 	}
-	else if (i == n_pipes)
+	else if (i == n_pipes && last_child_process(argv, fds, i) == -1)
 	{
-		file_fd = open_file(argv[len_argv - 1], OF_TR);
-		if file_fd == -1)
-		{
-			close_fds(fds);
-			err_exit();
-		}
-		dup2(fds[i][0], STDIN_FILENO);
-		dup2(file_fd, STDOUT_FILENO);
+		close_fds(fds);
+		err_exit();
 	}
 	else
 	{
-		dup2(fds[i][0], STDIN_FILENO);
-		dup2(fds[i][1], STDOUT_FILENO);
+		if (middle_child_process(fds, i) == -1)
+		{
+			close_fds(fds);
+			err_exit();
+		}
+	}
+	close_fds(fds);
+	return ;
+}
+
+static void	create_child_p(char **argv, int **fds, int n_pipes, char **envp)
+{
+	int	i;
+	int	p_id;
+
+	i = 0;
+	p_id = -1;
+	while (i < n_pipes)
+	{
+		p_id = fork();
+		if (p_id == -1)
+		{
+			close_fds(fds);
+			err_exit();
+		}
+		else if (p_id == 0)
+		{
+			child_p(argv, fds, n_pipes, i);
+			execute_program(argv[i], envp);
+		}
+		i++;
+	}
+	return ;
+}
+
+static void	wait_all_child_p(int n_pipes)
+{
+	int	i;
+
+	i = 0;
+	while (i < n_pipes)
+	{
+		wait(NULL)
+		i++;
 	}
 	return ;
 }
@@ -57,40 +126,16 @@ int	main(int argc, char **argv, char **envp)
 {
 	int	**fds;
 	int	n_pipes;
-	int	p_id;
-	int	i;
 
-	i = 0;
 	n_pipes = argc - 4;
 	if (argc >= 5)
 	{
-		if (check_hello_doc(argv[1]) == 0)
-			run_hello_doc_function();
 		fds = init_fds(n_pipes);
-		if (fds == NULL || init_pipes(pipes) == -1)
+		if (fds == NULL || init_pipes(fds) == -1)
 			err_exit();
-		while (i < n_pipes)
-		{
-			p_id = fork();
-			if (p_id == -1)
-			{
-				close_fds(fds);
-				err_exit();
-			}
-			else if (p_id == 0)
-				break;
-			i++;
-		}
-		if (p_id == 0)
-		{
-			child_p(argv, fds, n_pipes, i);
-			execute_program(argv[i], envp);
-		}
-		else
-		{
-			waitpid(p_id, NULL, 0);
-			parent_p(argv, fds, envp, i);
-		}
+		create_child_p(argv, fds, n_pipes, envp);
+		wait_all_child_p(n_pipes);
+		close_fds(fds);
 	}
 	return (0);
 }
