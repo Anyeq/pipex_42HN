c313838082459b22f1c2b374a238293bc0eebd22
asando <alexando17@yahoo.com>
2025-09-09 14:53:39 +0200

    continuing pipex_bonus, splitting the main code into utils

--- src/pipex_bonus.c
+++ src/pipex_bonus.c
@@ -6,60 +6,51 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 13:53:05 by asando            #+#    #+#             */
-/*   Updated: 2025/09/08 17:25:24 by asando           ###   ########.fr       */
+/*   Updated: 2025/09/09 14:52:57 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
 
-//control if npipe is 0
-static int	**init_fd(int npipe)
+static void	child_p(char **argv, int **fds, char **envp, int i)
 {
-	int	**fds;
-	int	i;
 	int	j;
-
-	i = 0;
+	int	file_fd;
+	int	len_argv;
 	j = 0;
-	fds = malloc(sizeof(int *) * (npipe + 1));
-	if (fds == NULL)
-		err_exit();
-	while (i < npipe)
-	{
-		fds[i] = malloc(sizeof(int) * 2);
-		if (fds[i] == NULL)
-			break ;
-		i++;
-	}
-	if (i == npipe)
-	{
-		fds[i] = NULL;
-		return (fds);
-	}
-	while (j < i)
-		free(fds[j++]);
-	free(fds);
-	return (NULL);
-}
 
-static int	init_pipes(int **fds)
-{
-	int	i;
-
-	i = 0;
-	while (fds[i])
+	file_fd = 0;
+	len_argv = 0;
+	while (argv[len_argv])
+		len_argv++;
+	if (i == 0)
 	{
-		if (pipe[fds[i]] == -1)
+		file_fd = open_file(argv[1], OF_RD);
+		if (file_fd == -1)
 		{
-			i = 0;
-			while (fds[i])
-				free(fds[i++]);
-			free(fds);
-			return (-1);
+			close_fds(fds);
+			err_exit();
 		}
-		i++;
+		dup2(file_fd, STDIN_FILENO);
+		dup2(fds[i][1], STDOUT_FILENO);
 	}
-	return (0);
+	else if (i == n_pipes)
+	{
+		file_fd = open_file(argv[len_argv - 1], OF_TR);
+		if file_fd == -1)
+		{
+			close_fds(fds);
+			err_exit();
+		}
+		dup2(fds[i][0], STDIN_FILENO);
+		dup2(file_fd, STDOUT_FILENO);
+	}
+	else
+	{
+		dup2(fds[i][0], STDIN_FILENO);
+		dup2(fds[i][1], STDOUT_FILENO);
+	}
+	return ;
 }
 
 int	main(int argc, char **argv, char **envp)
@@ -78,18 +69,23 @@ int	main(int argc, char **argv, char **envp)
 		fds = init_fds(n_pipes);
 		if (fds == NULL || init_pipes(pipes) == -1)
 			err_exit();
-		// for non hello_doc case
 		while (i < n_pipes)
 		{
 			p_id = fork();
 			if (p_id == -1)
+			{
+				close_fds(fds);
 				err_exit();
+			}
 			else if (p_id == 0)
 				break;
 			i++;
 		}
 		if (p_id == 0)
-			child_p(argv, fds, envp, i);
+		{
+			child_p(argv, fds, n_pipes, i);
+			execute_program(argv[i], envp);
+		}
 		else
 		{
 			waitpid(p_id, NULL, 0);
---
+++ src/pipex_bonus_utils.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   pipex_bonus_utils.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/09 14:50:01 by asando            #+#    #+#             */
+/*   Updated: 2025/09/09 14:52:39 by asando           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "pipex_bonus.h"
+
+static int	**init_fd(int npipe)
+{
+	int	**fds;
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	fds = malloc(sizeof(int *) * (npipe + 1));
+	if (fds == NULL)
+		return (NULL);
+	while (i < npipe)
+	{
+		fds[i] = malloc(sizeof(int) * 2);
+		if (fds[i] == NULL)
+			break ;
+		i++;
+	}
+	if (i == npipe)
+	{
+		fds[i] = NULL;
+		return (fds);
+	}
+	while (j < i)
+		free(fds[j++]);
+	free(fds);
+	return (NULL);
+}
+
+static void	close_fds(int **fds)
+{
+	int	i;
+
+	i = 0;
+	while (fds[i])
+	{
+		close(fds[i][0]);
+		close(fds[i][1]);
+		free(fds[i++]);
+	}
+	free(fds);
+	return ;
+}
+
+static int	init_pipes(int **fds)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	j = 0;
+	while (fds[i])
+	{
+		if (pipe[fds[i]] == -1)
+		{
+			while (j < i)
+			{
+				close(fds[j][0]);
+				close(fds[j][1]);
+				free(fds[j++]);
+			}
+			while (fds[i])
+				free(fds[i++]);
+			free(fds);
+			return (-1);
+		}
+		i++;
+	}
+	return (0);
+}
+
+static int	open_file(char *argv, t_offlag of_flag)
+{
+	int	file_fd;
+
+	file_fd = 0;
+	if (of_flag & OF_APP)
+		file_fd = open(argv, O_WRONLY | O_CREAT | O_APPEND, 0644);
+	else if (of_flag & OF_TR)
+		file_fd = open(argv, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	else if (of_flag & OF_RD)
+		file_fd = open(argv, O_RDONLY, 0644);
+	return (file_fd);
+}
