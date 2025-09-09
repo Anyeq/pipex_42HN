b73f6de387405d341bbda20d06e00ad69047e932
asando <alexando17@yahoo.com>
2025-09-09 17:16:27 +0200

    adding header

--- includes/pipex_bonus.h
+++ includes/pipex_bonus.h
@@ -6,11 +6,25 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/07 13:53:46 by asando            #+#    #+#             */
-/*   Updated: 2025/09/07 13:54:40 by asando           ###   ########.fr       */
+/*   Updated: 2025/09/09 14:38:56 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PIPEX_BONUS_H
 # define PIPEX_BONUS_H
+
+# include <stdlib.h>
+# include <stdio.h>
+# include <unistd.h>
+# include <fcntl.h>
+# include <sys/wait.h>
+
+typedef enum of_flag
+{
+	OF_RD = 1 << 0,
+	OF_TR = 1 << 1,
+	OF_APP = 1 << 2
+}	t_offlag;
+
 #endif
 
