f4577b0fd9ca58bcd36c4a0d70f2c17fe55dd322
asando <alexando17@yahoo.com>
2025-09-11 18:09:15 +0200

    deleting repeting code from libft in get_next_line

--- includes/get_next_line_bonus.h
+++ includes/get_next_line_bonus.h
@@ -6,7 +6,7 @@
 /*   By: asando <asando@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/06 09:41:04 by asando            #+#    #+#             */
-/*   Updated: 2025/05/19 14:53:42 by asando           ###   ########.fr       */
+/*   Updated: 2025/09/11 18:07:38 by asando           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,13 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <limits.h>
+# include "libft.h"
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 8
 # endif
 
 char	*get_next_line(int fd);
-char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlcpy(char *dest, const char *src, size_t len);
-size_t	ft_strlen(const char *str);
-char	*ft_strchr(const char *str, int c);
 ssize_t	read_fd(int fd, char *buff, char **storage);
 int		err_check(int fd, char **storage);
 char	*ft_find_line(char **storage, ssize_t byte_read);
